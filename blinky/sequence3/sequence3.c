7a12b9e3a22310107473f5a9baa803f19dc5efd2
Trevor Hennessy <thennessy@savagenorth.ca>
2011-10-29 15:51:22 -0400

    ok now we're really finished cleanup

---
+++ blinky/sequence3/sequence3.c
@@ -0,0 +1,30 @@
+/* sequence3.c
+ *
+ * Description: Blinks left by 2 shifts 
+ * Trevor Hennessy
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void)
+{
+	//set PORTD for output
+	DDRD = 0xFF;
+    
+		while(1) {
+			
+			for(int i = 1; i <= 128; i <<= 2) //loop left on the leds
+			{
+				PORTD = i;
+				_delay_loop_2(30000);
+			}
+
+			for(int i = 128; i >= 1; i >>= 2) //loop right on the leds
+			{
+				PORTD = i;
+				_delay_loop_2(30000);
+			}
+		}
+		return 1;
+}
