0ac693b2f016d6c16c0d081a4014ecdaddcf51a5
Trevor Hennessy <thennessy@savagenorth.ca>
2011-10-31 10:31:41 -0400

    added the initial bits of my latest assignment

---
+++ pushbutton/push.c
@@ -0,0 +1,18 @@
+/* Trevor Hennessy
+ *
+ * Program for counting via a push button
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+#include "ledLoops.h"
+
+int main ( void )
+{
+
+    DDRD = 0xFF;
+    DDRB = 0x00;
+
+
+}
