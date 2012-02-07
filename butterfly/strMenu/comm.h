8355b80ca3c038600f2a6dc50e97edfd0c739270
Trevor Hennessy <thennessy@savagenorth.ca>
2012-02-07 10:01:29 -0500

    added new project for comparing strings rather than chars

---
+++ butterfly/strMenu/comm.h
@@ -0,0 +1,26 @@
+// PC_Comm.h
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define F_CPU 1000000UL
+#include <util/delay.h>
+#include <compat/deprecated.h>
+#include <string.h>
+
+#include <stdlib.h>
+#include <avr/pgmspace.h>
+
+#include "demo.h"
+
+#define MAX_STR_LEN 24
+
+void OSCCAL_calibration ( void );
+
+void USARTinit ( void );
+char isCharAvailable ( void );
+char receiveChar ( void );
+void sendChar ( char data ) ;
+void sendString ( char *s );
+
+void sendFString ( const char *s );
+
+void getString ( char *string, unsigned char *flag );
---
+++ butterfly/strMenu/demo.c
@@ -0,0 +1,143 @@
+/* demo.c
+ * 
+ * Trevor Hennessy
+ */
+
+#include "comm.h"
+
+void initializer ( void )
+{
+	// Calibrate the oscillator:
+    OSCCAL_calibration ();   
+	// Initialize the USART
+	USARTinit ();
+	
+	// say hello
+	//sendString ( "PC_Comm.c ready to communicate.\r" );   	 
+	// identify yourself specifically
+	//sendString ( "You are talking to the PC_Comm demo.\r" );
+
+    menu ();
+}
+
+void menu ( void )
+{
+    sendChar ( '\r' );
+    sendString ( "Menu\r" );
+    sendString ( "a. Add\r" );
+    sendString ( "b. Sub\r" );
+    sendString ( "c. Mult\r" );
+    sendString ( "d. Integer division\r" );
+    sendString ( "e. Decimal division\r" );
+    sendChar ( '\r' );
+}
+
+void parseInput ( char *s )
+{
+    if ( *( s + 0 ) == '\0' )
+        s = &*( s + 1 );
+
+    long num1, num2;
+    unsigned char i = 0;
+    char *ptr, *temp = malloc ( sizeof(*temp) * 18 );
+    memset ( temp, 0, ( sizeof(*temp) * 18 ) );
+
+    switch ( *( s + 0 ) )
+    {
+        case 'a':
+            num1 = getNum ();
+            num2 = getNum ();
+            ptr = ltoa ( (num1 + num2), temp, 10 );
+            break;
+        case 'b':
+            num1 = getNum ();
+            num2 = getNum ();
+            ptr = ltoa ( (num1 - num2), temp, 10 );
+            break;
+        case 'c':
+            num1 = getNum ();
+            num2 = getNum ();
+            ptr = ltoa ( (num1 * num2), temp, 10 );
+            break;
+        case 'd':
+            num1 = getNum ();
+            num2 = getNum ();
+            ptr = ltoa ( (num1 / num2), temp, 10 );
+            break;
+        case 'e':
+            num1 = getNum ();
+            num2 = getNum ();
+            num1 = num1 * 1000;
+            ptr = ltoa ( (num1 / num2), temp, 10 );
+                
+                while ( i < 18 )
+                {
+                    if ( *( ptr + i ) == '\0' )
+                    {
+                        for ( int j = i; j > ( i - 3 ); j-- )
+                        {
+                            *( ptr + j ) = *( ptr + (j - 1) );
+                        }
+                        *( ptr + ( i - 3 ) ) = '.';
+                        *( ptr + ( i + 1 ) ) = '\0';
+                        break;
+                    }
+                    i++;
+                }
+
+            break;
+        default:
+            sendString ( "ERROR: YOU SO STUPID\7\r" );
+            break;
+    }
+
+    switch ( *( s + 0 ) )
+    {
+        case 'a':
+        case 'b':
+        case 'c':
+        case 'd':
+        case 'e':
+            sendString ( "Result: " );
+            sendString ( ptr );
+            break;
+    }
+    
+    sendChar ( '\r' );
+
+    free ( temp );
+}
+
+int getNum ( void )
+{
+    int num;
+    unsigned char i = 0;
+    char *s = malloc ( sizeof(*s) * MAX_STR_LEN );
+    void *f;
+    memset ( s, 0, ( sizeof(*s) * MAX_STR_LEN ) );
+
+    sendString ( "Input a number:\r" );
+
+    getString ( s, f );
+
+    while ( 1 )
+    {
+        if ( *( s + i ) == '\n' )
+        {
+            *( s + i ) = '\0';
+            break;
+        }
+        else if ( *( s + i ) == '\r' )
+        {
+            *( s + i ) = '\0';
+            break;
+        }
+        i++;
+    }
+
+    num = atoi ( s );
+
+    free ( s );
+
+    return num;
+}
