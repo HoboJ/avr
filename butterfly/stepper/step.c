e3b629cc817f2129f62f59e5c9dd221150e1cffb
Trevor Hennessy <thennessy@savagenorth.ca>
2012-03-14 10:32:51 -0400

    stepper 3.5 degree

--- butterfly/stepper/step.c
+++ butterfly/stepper/step.c
@@ -13,13 +13,14 @@ int main ( void )
     DDRD = 0xFF;
     DDRB = 0x00;
 
-    int step[] = {0x03, 0x06, 0x0C, 0x09};
+    //int step[] = {0x03, 0x06, 0x0C, 0x09};
+    int step[] = {0x03, 0x02, 0x06, 0x04, 0x0C, 0x08, 0x09, 0x01};
 
     while ( 1 )
     {
         if ( PINB == 0x00 )
         {
-            for ( int i = 0; i < 4; i++ )
+            for ( int i = 0; i < 7; i++ )
             {
                 PORTD = step[i];
                 _delay_ms ( 100 );
@@ -27,7 +28,7 @@ int main ( void )
         }
         else if ( PINB == 0x01 )
         {
-            for ( int i = 3; i > -1; i-- )
+            for ( int i = 7; i > -1; i-- )
             {
                 PORTD = step[i];
                 _delay_ms ( 100 );
