86a81cb30601e0040981a7445e0028b82d87137a
HoboJ <thennessy@savagenorth.ca>
2012-11-01 11:16:44 -0400

    added reverse dct

--- termProgs/dct/idct.c
+++ termProgs/dct/idct.c
@@ -1,9 +1,9 @@
-/* dct.c
+/* idct.c
  * 
  * T. Hennessy
  *
- * Description: command line utility for calculating the
- * DCT values of a 8x8 block
+ * Description: command line utility for reversing
+ * a DCT result
  */
 
 #include <stdio.h>
@@ -12,7 +12,7 @@
 
 #define PI 3.14159265f
 
-float dctCalc ( int nums[8][8], int u, int v );
+float idctCalc ( int nums[8][8], int u, int v );
 
 int main (int argc, char *argv[])
 {
@@ -40,46 +40,31 @@ int main (int argc, char *argv[])
             }
         }
 
+        final = idctCalc ( nums, u, v );
+        printf ( "%f\n", final );
+
+        /*
         for ( u = 0; u < 8; u++ )
         {
             for ( v = 0; v < 8; v++ )
             {
-                final = dctCalc ( nums, u, v );
+                final = idctCalc ( nums, u, v );
                 if ( v == 7 )
                     printf ( "%f\n", final );
                 else
                     printf ( "%f ", final );
             }
         }
+        */
     }
 
     return 0;
 }
 
-float dctCalc ( int nums[8][8], int u, int v )
+float idctCalc ( int nums[8][8], int u, int v )
 {
     float results[8][8], final = 0, cu, cv;        
     int x, y;
-
-    /* Calculate new array values based upon u and v coordinate 
-     */
-    for ( x = 0; x < 8; x++ )
-    {
-        for ( y = 0; y < 8; y++ )
-        {
-            results[x][y] = nums[x][y] * cosf ( ( ( 2 * x + 1 ) * u * PI ) /16 ) * cosf ( ( ( 2 * y + 1 ) * v * PI ) / 16 );
-        }
-    }
-
-    /* Perform summation on array of calculated values
-     */
-    for ( x = 0; x < 8; x++ )
-    {
-        for ( y = 0; y < 8; y++ )
-        {
-            final = final + results[x][y];
-        }
-    }
 
     /* Decide if u or v are greater than 0 and perform proper caluclation
      * store value in cu and cv respectively
@@ -102,9 +87,29 @@ float dctCalc ( int nums[8][8], int u, int v )
         cv = ( 1/sqrtf ( 2 ) ) / 2;
     }
 
+    /* Calculate new array values based upon u and v coordinate 
+     */
+    for ( x = 0; x < 8; x++ )
+    {
+        for ( y = 0; y < 8; y++ )
+        {
+            results[x][y] = nums[x][y] * cu * cv * cosf ( ( ( 2 * x + 1 ) * u * PI ) /16 ) * cosf ( ( ( 2 * y + 1 ) * v * PI ) / 16 );
+        }
+    }
+
+    /* Perform summation on array of calculated values
+     */
+    for ( x = 0; x < 8; x++ )
+    {
+        for ( y = 0; y < 8; y++ )
+        {
+            final = final + results[x][y];
+        }
+    }
+
     /* Calculate the final value at coordinate based upon u and v
      */
-    final = cu * cv * final;
+    //final = cu * cv * final;
 
     return final;
 }
